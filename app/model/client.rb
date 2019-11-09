18d7eaedb2adc28ff877f010ea22ea1415f0e05e
kevinspasiuk <kevin_spasiuk@hotmail.com>
2019-11-08 22:02:56 -0300

    Client repositroy created

--- app/model/client.rb
+++ app/model/client.rb
@@ -1,5 +1,9 @@
+require 'active_model'
+
 class Client
-  attr_accessor :id, :name, :phone, :address
+  include ActiveModel::Validations
+
+  attr_accessor :id, :name, :phone, :address, :updated_on, :created_on
 
   def initialize; end
 end
