ed9d17df53b280c9519daa6d3e1fbd4447f47e65
edjus <justo.edson@gmail.com>
2019-11-14 01:09:28 -0300

    Delivery is valid when username is not blank

--- app/model/delivery.rb
+++ app/model/delivery.rb
@@ -1,5 +1,16 @@
+require 'active_model'
+
 class Delivery
+  include ActiveModel::Validations
+
   attr_accessor :id, :username, :updated_on, :created_on
 
-  def initialize(data = {}) end
+  validates :username, presence: true
+
+  def initialize(data = {})
+    @id = data['id']
+    @username = data['username']
+    @updated_on = data['updated_on']
+    @created_on = data['created_on']
+  end
 end
--- spec/delivery_spec.rb
+++ spec/delivery_spec.rb
@@ -9,4 +9,12 @@ describe Delivery do
     it { is_expected.to respond_to(:created_on) }
     it { is_expected.to respond_to(:updated_on) }
   end
+
+  describe 'valid?' do
+    it 'should be valid when username not blank' do
+      delivery = described_class.new('username' => 'pepemoto')
+      expect(delivery.valid?).to eq true
+      expect(delivery.errors.empty?).to eq true
+    end
+  end
 end
