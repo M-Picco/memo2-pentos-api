616de442f4e73de2720f85216dbb9d3da473eb4b
edjus <justo.edson@gmail.com>
2019-11-15 00:18:32 -0300

    Test delivery is valid when size is five characters

--- spec/delivery_spec.rb
+++ spec/delivery_spec.rb
@@ -17,10 +17,16 @@ describe Delivery do
       expect(delivery.errors.empty?).to eq true
     end
 
-    it 'should be invalid when username size es less than five characters' do
+    it 'should be invalid when username size is less than five characters' do
       delivery = described_class.new('username' => 'pepe')
       expect(delivery.valid?).to eq false
       expect(delivery.errors).to have_key(:username)
+    end
+
+    it 'should be valid when username size is five characters' do
+      delivery = described_class.new('username' => 'pepes')
+      expect(delivery.valid?).to eq true
+      expect(delivery.errors.empty?).to eq true
     end
   end
 end
