6d49e8c8ffd8aa2681cefb8f4af4d480421a5751
M-Picco <martinalejandropicco1996@gmail.com>
2019-11-15 11:39:42 -0300

    Introduce FailedSaveOperationError and use it instead of if/else in the controller

---
+++ app/errors/failed_save_operation_error.rb
@@ -0,0 +1,17 @@
+class FailedSaveOperationError < StandardError
+  def initialize(entity)
+    super
+    @entity = entity
+  end
+
+  def message
+    extract_first_error(@entity)
+  end
+
+  def extract_first_error(entity)
+    return '' if entity.errors.empty?
+
+    # Ex: entity.errors.messages = [:symbol, ["the error"]]
+    entity.errors.messages.first[1].first
+  end
+end
