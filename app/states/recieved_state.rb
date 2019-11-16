4bd13a84ebe940681e2a5ab1ebb5c614734f424c
kevinspasiuk <kevin_spasiuk@hotmail.com>
2019-11-16 13:23:04 -0300

    State Inheritance refactor

--- app/states/recieved_state.rb
+++ app/states/recieved_state.rb
@@ -1,15 +1,7 @@
-class RecievedState
-  attr_reader :state_name
+require_relative 'state'
 
+class RecievedState < State
   def initialize
     @state_name = 'recibido'
-  end
-
-  def name?(other_state)
-    other_state == @state_name
-  end
-
-  def ==(other)
-    other.name?(state_name)
   end
 end
