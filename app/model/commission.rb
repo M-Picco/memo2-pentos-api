cc670f9d8a7430b8375529d970aa75b3038b109b
edjus <justo.edson@gmail.com>
2019-11-16 23:30:41 -0300

    Commission is valida when order_cost is positive number

--- app/model/commission.rb
+++ app/model/commission.rb
@@ -1,8 +1,15 @@
+require 'active_model'
+
 class Commission
+  include ActiveModel::Validations
+
   attr_accessor :id, :amount, :order_cost
 
-  def initialize(order_cost)
-    @order_cost = order_cost
-    @amount = 0.05 * order_cost
+  validates :order_cost, length: { minimum: 0 }
+
+  def initialize(data = { 'order_cost' => 0 })
+    @id = data['id']
+    @order_cost = data['order_cost']
+    @amount = 0.05 * @order_cost
   end
 end
--- spec/commission_spec.rb
+++ spec/commission_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Commission do
-  subject(:commision) { described_class.new(100) }
+  subject(:commision) { described_class.new('order_cost' => 100) }
 
   describe 'model' do
     it { is_expected.to respond_to(:id) }
@@ -9,15 +9,24 @@ describe Commission do
     it { is_expected.to respond_to(:order_cost) }
   end
 
+  describe 'valid?' do
+    it 'should de valid when order_cost is positive number' do
+      commision = described_class.new
+      expect(commision.valid?).to eq true
+      expect(commision.errors.empty?).to eq true
+    end
+  end
+
   describe 'ammount' do
     it 'should be 5 when order cost is 100' do
-      commision = described_class.new(100)
+      commision = described_class.new('order_cost' => 100)
       expect(commision.amount).to eq(5)
     end
 
     it 'should be 5% of the order cost' do
-      commision = described_class.new(200)
-      expect(commision.amount).to eq(10)
+      order_cost = 200
+      commision = described_class.new('order_cost' => order_cost)
+      expect(commision.amount).to eq(order_cost * 0.05)
     end
   end
 end
