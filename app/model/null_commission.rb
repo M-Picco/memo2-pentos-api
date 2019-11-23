6db3e242478ec742eab27322aec45e8853941181
M-Picco <martinalejandropicco1996@gmail.com>
2019-11-23 14:57:39 -0300

    Weather for NullCommission

--- app/model/null_commission.rb
+++ app/model/null_commission.rb
@@ -1,7 +1,12 @@
 require_relative '../errors/order_not_delivered_error'
+require_relative './weather/non_rainy_weather'
 
 class NullCommission
   def amount
     raise OrderNotDeliveredError
+  end
+
+  def weather
+    NonRainyWeather.new
   end
 end
--- spec/null_commission_spec.rb
+++ spec/null_commission_spec.rb
@@ -5,11 +5,20 @@ describe NullCommission do
 
   describe 'model' do
     it { is_expected.to respond_to(:amount) }
+    it { is_expected.to respond_to(:weather) }
   end
 
   describe 'amount' do
     it 'should raise an OrderNotDeliveredError' do
       expect { commision.amount }.to raise_error(OrderNotDeliveredError)
+    end
+  end
+
+  describe 'weather' do
+    it 'should return NonRainyWeather as default' do
+      weather = commision.weather
+
+      expect(weather).to be_a(NonRainyWeather)
     end
   end
 end
