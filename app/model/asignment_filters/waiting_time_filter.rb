da62978712ad3b04c9d9eb434ec9696a4e0b8869
kevinspasiuk <kevin_spasiuk@hotmail.com>
2019-11-27 21:19:28 -0300

    Wating time filters add wating time to delivery

---
+++ app/model/asignment_filters/waiting_time_filter.rb
@@ -0,0 +1,11 @@
+class WaitingTimeFilter
+  def apply(deliveries, _orders)
+    repository = OrderRepository.new
+    date = Time.now.round
+    deliveries.map do |delivery|
+      last_order = repository.last_on_delivery_by(delivery.username)
+      delivery.waiting_time = (date - last_order.on_delivery_time) / 60.0
+    end
+    deliveries
+  end
+end
