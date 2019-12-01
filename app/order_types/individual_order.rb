2071f08d2efc596ceaabe233a26ee5325288d64d
edjus <justo.edson@gmail.com>
2019-12-01 01:51:46 -0300

    Individual order created with correct atributes values

--- app/order_types/individual_order.rb
+++ app/order_types/individual_order.rb
@@ -1,5 +1,10 @@
 class IndividualOrderType
   attr_reader :type_name, :size, :cost, :base_time
 
-  def initialize; end
+  def initialize
+    @type_name = 'menu_individual'
+    @size = 1
+    @cost = 100
+    @base_time = 10
+  end
 end
--- spec/order_types/individual_order_type_spec.rb
+++ spec/order_types/individual_order_type_spec.rb
@@ -6,7 +6,19 @@ describe IndividualOrderType do
 
   describe 'model' do
     it { is_expected.to respond_to(:type_name) }
+    it { is_expected.to respond_to(:size) }
     it { is_expected.to respond_to(:cost) }
     it { is_expected.to respond_to(:base_time) }
+  end
+
+  describe 'create' do
+    # rubocop:disable RSpec/MultipleExpectations
+    it 'created with correct attributes values' do
+      expect(type.type_name).to eq 'menu_individual'
+      expect(type.size).to eq 1
+      expect(type.cost).to eq 100
+      expect(type.base_time).to eq 10
+    end
+    # rubocop:enable RSpec/MultipleExpectations
   end
 end
